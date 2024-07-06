1eff247176677154371a344691d74e45eaab8b08
adfish <fischbachadam92@gmail.com>
2024-07-06 21:05:13 +0200

    implement algorithm for gaining data in order to generate a report Algorithm consists in getting data for a previous month, check whether the data for current month has been collected. If yes, the data should be retrieved from db or conduct measurements else

--- src/main/java/com/am/telegram/groupstat/user/report/statistic/StatisticService.java
+++ src/main/java/com/am/telegram/groupstat/user/report/statistic/StatisticService.java
@@ -6,6 +6,7 @@ import com.am.telegram.groupstat.user.report.group.GroupRepository;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -23,15 +24,21 @@ public class StatisticService {
     }
 
     public List<GroupStatistic> generateCurrentReport() {
-        try(Connection connection = ds.getConnection()){
+        try (Connection connection = ds.getConnection()) {
             List<GroupDTO> groupDTOs = groupRepository.findAllGroups(connection);
-            Map<String, GroupMonthStatisticDTO> currentStatistics = statisticRepository.measureCurrentCountOfUsers(groupDTOs);
-            Map<String, GroupMonthStatisticDTO> previousMonthStatistics = statisticRepository.getPreviousMonthStatistics(groupDTOs, connection);
+
+            Map<String, GroupMonthStatisticDTO> currentStatistics;
+            if (statisticRepository.existMeasurementsForDate(LocalDate.now(), connection)) {
+                currentStatistics = statisticRepository.findMeasurementsForDate(LocalDate.now(), connection);
+            } else {
+                currentStatistics = statisticRepository.measureCurrentCountOfUsers(groupDTOs);
+                statisticRepository.persistMeasurements(currentStatistics.values(), connection);
+            }
+            Map<String, GroupMonthStatisticDTO> previousMonthStatistics = statisticRepository.getPreviousMonthStatistics(connection);
             List<GroupStatistic> groupStatistics = new ArrayList<>();
 
             for (GroupDTO groupDTO : groupDTOs) {
-                groupStatistics.add(new GroupStatistic(groupDTO, previousMonthStatistics.get(groupDTO.getInternalName()),
-                        currentStatistics.get(groupDTO.getInternalName())));
+                groupStatistics.add(new GroupStatistic(groupDTO, previousMonthStatistics.get(groupDTO.getInternalName()), currentStatistics.get(groupDTO.getInternalName())));
             }
 
             return groupStatistics;
@@ -40,8 +47,6 @@ public class StatisticService {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-
-
     }
 
 }
