dcc4a08b686dbc873017becb42a1ed6db6ae036f
adfish <fischbachadam92@gmail.com>
2024-08-13 21:00:40 +0200

    apply code style

--- src/main/java/com/am/telegram/groupstat/logic/report/ReportService.java
+++ src/main/java/com/am/telegram/groupstat/logic/report/ReportService.java
@@ -24,88 +24,92 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ReportService {
 
-    private AtomicBoolean isReportBeingGenerated = new AtomicBoolean(false);
-    private List<ReportSubscriber> reportSubscribers = Collections.synchronizedList(new ArrayList<>());
+  private AtomicBoolean isReportBeingGenerated = new AtomicBoolean(false);
+  private List<ReportSubscriber> reportSubscribers =
+      Collections.synchronizedList(new ArrayList<>());
 
-    private final StatisticService statisticService;
+  private final StatisticService statisticService;
 
-    public ReportService(StatisticService statisticService) {
-        this.statisticService = statisticService;
+  public ReportService(StatisticService statisticService) {
+    this.statisticService = statisticService;
+  }
+
+  public void subscribe(ReportSubscriber reportSubscriber) throws InterruptedException {
+    if (reportSubscribers.contains(reportSubscriber)) {
+      return;
     }
 
-    public void subscribe(ReportSubscriber reportSubscriber) throws InterruptedException {
-        if (reportSubscribers.contains(reportSubscriber)) {
-            return;
+    System.out.println("subscriber is regestred");
+    reportSubscribers.add(reportSubscriber);
+    if (isReportAlreadyGenerated()) {
+      System.out.println("Report is already subscribed");
+      notifySubscribers();
+    } else if (!isReportBeingGenerated.get()) {
+      Thread thread = new Thread(this::generateReport);
+      thread.start();
+    }
+  }
+
+  private void notifySubscribers() {
+    System.out.println("Notify users");
+    File report = new File(currentReportName());
+    for (ReportSubscriber subscriber : reportSubscribers) {
+      subscriber.update(report);
+    }
+    reportSubscribers = Collections.synchronizedList(new ArrayList<>());
+  }
+
+  private void generateReport() {
+    isReportBeingGenerated.set(true);
+    System.out.println("Generate report");
+
+    try (Workbook workbook = new XSSFWorkbook()) {
+      List<GroupStatistic> groupStatistics = statisticService.generateCurrentReport();
+      Map<String, List<GroupStatistic>> groupStatisticsPerCategory =
+          groupStatistics.stream().collect(groupingBy(GroupStatistic::category));
+      // TODO fix month
+      Sheet sheet = workbook.createSheet("juli");
+      Row header = sheet.createRow(0);
+      header.createCell(0).setCellValue("Город");
+      header.createCell(1).setCellValue("Настоящее значение");
+      header.createCell(2).setCellValue("Предыдущее значение");
+      header.createCell(3).setCellValue("Разница");
+
+      int rowNumber = 1;
+      for (Map.Entry<String, List<GroupStatistic>> category :
+          groupStatisticsPerCategory.entrySet()) {
+        Row categoryRow = sheet.createRow(rowNumber++);
+        categoryRow.createCell(0).setCellValue(category.getKey());
+        for (GroupStatistic groupStatistic : category.getValue()) {
+          Row groupRow = sheet.createRow(rowNumber++);
+          groupStatistic.writeInRow(groupRow);
         }
+      }
 
-        System.out.println("subscriber is regestred");
-        reportSubscribers.add(reportSubscriber);
-        if (isReportAlreadyGenerated()) {
-            System.out.println("Report is already subscribed");
-            notifySubscribers();
-        } else if (!isReportBeingGenerated.get()) {
-            Thread thread = new Thread(this::generateReport);
-            thread.start();
-        }
+      save(workbook);
+    } catch (Exception e) {
+      e.printStackTrace();
+    } finally {
+      isReportBeingGenerated.set(false);
     }
+    notifySubscribers();
+  }
 
-    private void notifySubscribers() {
-        System.out.println("Notify users");
-        File report = new File(currentReportName());
-        for (ReportSubscriber subscriber : reportSubscribers) {
-            subscriber.update(report);
-        }
-        reportSubscribers = Collections.synchronizedList(new ArrayList<>());
+  private boolean isReportAlreadyGenerated() {
+    var reportName =
+        LocalDate.now().format(DateTimeFormatter.ofPattern("ddMMyyyy")) + "-report.xlsx";
+    return Files.exists(Path.of(reportName));
+  }
+
+  private void save(Workbook workbook) {
+    try (FileOutputStream fileOut = new FileOutputStream(currentReportName())) {
+      workbook.write(fileOut);
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+  }
 
-    private void generateReport() {
-        isReportBeingGenerated.set(true);
-        System.out.println("Generate report");
-
-        try (Workbook workbook = new XSSFWorkbook()) {
-            List<GroupStatistic> groupStatistics = statisticService.generateCurrentReport();
-            Map<String, List<GroupStatistic>> groupStatisticsPerCategory = groupStatistics.stream().collect(groupingBy(GroupStatistic::category));
-
-            Sheet sheet = workbook.createSheet("juli");
-            Row header = sheet.createRow(0);
-            header.createCell(0).setCellValue("Город");
-            header.createCell(1).setCellValue("Настоящее значение");
-            header.createCell(2).setCellValue("Предыдущее значение");
-            header.createCell(3).setCellValue("Разница");
-
-            int rowNumber = 1;
-            for (Map.Entry<String, List<GroupStatistic>> category : groupStatisticsPerCategory.entrySet()) {
-                Row categoryRow = sheet.createRow(rowNumber++);
-                categoryRow.createCell(0).setCellValue(category.getKey());
-                for (GroupStatistic groupStatistic : category.getValue()) {
-                    Row groupRow = sheet.createRow(rowNumber++);
-                    groupStatistic.writeInRow(groupRow);
-                }
-            }
-
-            save(workbook);
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            isReportBeingGenerated.set(false);
-        }
-        notifySubscribers();
-    }
-
-    private boolean isReportAlreadyGenerated() {
-        var reportName = LocalDate.now().format(DateTimeFormatter.ofPattern("ddMMyyyy")) + "-report.xlsx";
-        return Files.exists(Path.of(reportName));
-    }
-
-    private void save(Workbook workbook) {
-        try (FileOutputStream fileOut = new FileOutputStream(currentReportName())) {
-            workbook.write(fileOut);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private String currentReportName() {
-        return LocalDate.now().format(DateTimeFormatter.ofPattern("ddMMyyyy")) + "-report.xlsx";
-    }
+  private String currentReportName() {
+    return LocalDate.now().format(DateTimeFormatter.ofPattern("ddMMyyyy")) + "-report.xlsx";
+  }
 }
