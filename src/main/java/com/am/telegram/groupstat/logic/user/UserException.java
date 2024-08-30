6373f6e920455251b4503d3331c269da4b48cf30
adfish <fischbachadam92@gmail.com>
2024-08-30 19:34:43 +0200

    call super method to fix exception

--- src/main/java/com/am/telegram/groupstat/logic/user/UserException.java
+++ src/main/java/com/am/telegram/groupstat/logic/user/UserException.java
@@ -1,5 +1,7 @@
 package com.am.telegram.groupstat.logic.user;
 
 public class UserException extends RuntimeException {
-  public UserException(String message, Exception e) {}
+  public UserException(String message, Exception e) {
+    super(message, e);
+  }
 }
