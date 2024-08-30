4fb79e02cc3ede2ea054a7f88aa99a621bf08157
adfish <fischbachadam92@gmail.com>
2024-08-30 22:35:58 +0200

    adjust imports

--- src/main/java/com/am/telegram/groupstat/logic/report/statistic/StatisticRepository.java
+++ src/main/java/com/am/telegram/groupstat/logic/report/statistic/StatisticRepository.java
@@ -10,10 +10,7 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
