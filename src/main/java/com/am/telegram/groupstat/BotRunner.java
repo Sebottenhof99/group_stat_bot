70bbb534ad84c2cf991e9ca035df78817feab756
Adam Fischbach <adam.fischbach@sys.aok.de>
2024-08-30 08:28:19 +0200

    general clean up

--- src/main/java/com/am/telegram/groupstat/BotRunner.java
+++ src/main/java/com/am/telegram/groupstat/BotRunner.java
@@ -1,29 +1,20 @@
 package com.am.telegram.groupstat;
 
-import com.am.telegram.groupstat.logic.report.ReportService;
 import com.pengrad.telegrambot.TelegramBot;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 
 public class BotRunner implements ApplicationRunner {
-
-  private static Logger log = LogManager.getLogger(BotRunner.class);
-
   private final TelegramBot bot;
   private final StatUpdateListener updateListener;
-  private final ReportService reportService;
 
-  public BotRunner(
-      TelegramBot bot, StatUpdateListener updateListener, ReportService reportService) {
+  public BotRunner(TelegramBot bot, StatUpdateListener updateListener) {
     this.bot = bot;
     this.updateListener = updateListener;
-    this.reportService = reportService;
   }
 
   @Override
-  public void run(ApplicationArguments args) throws Exception {
+  public void run(ApplicationArguments args) {
     bot.setUpdatesListener(updateListener);
   }
 }
