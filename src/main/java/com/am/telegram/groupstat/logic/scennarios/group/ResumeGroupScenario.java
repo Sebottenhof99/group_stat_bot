7d87a0b7bc20d87ce307958d4de362857c89bd14
adfish <fischbachadam92@gmail.com>
2024-08-30 20:07:32 +0200

    remove unused classes

--- src/main/java/com/am/telegram/groupstat/logic/scennarios/group/ResumeGroupScenario.java
+++
@@ -1,8 +0,0 @@
-package com.am.telegram.groupstat.logic.scennarios.group;
-
-import com.am.telegram.groupstat.logic.scennarios.Scenario;
-
-public class ResumeGroupScenario implements Scenario {
-  @Override
-  public void execute(long chatId) {}
-}
